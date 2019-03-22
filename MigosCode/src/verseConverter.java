07f1785e31f516f89d7890fd809528fe7c982c11
Vishnu Srinivas <vishnu.8913@gmail.com>
2019-03-22 00:41:34 -0700

    DONE

--- MigosCode/src/verseConverter.java
+++ MigosCode/src/verseConverter.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class verseConverter {
 	private ArrayList<String> lyrics;
 	private POStoWord converter;
+	ArrayList<String> toReturn = new ArrayList<String>();
 
 	public verseConverter(ArrayList<String> lyrics, String who) throws Exception {
 		this.lyrics = lyrics;
@@ -10,15 +11,66 @@ public class verseConverter {
 	}
 
 	public ArrayList<String> getConverted() {
-		ArrayList<String> toReturn = new ArrayList<String>();
 		for (int i = 0; i < lyrics.size(); i++) {
+			
+			if(lyrics.get(i).equalsIgnoreCase("adlib"))
+			{
+				toReturn.add(getAdlib(i));
+			}
+			else
+			{
+			
 
 			String POS = lyrics.get(i);
 			String word = converter.getWord(POS);
 
 			toReturn.add(word);
+			}
+			
 		}
 
 		return toReturn;
 	}
+	
+	public String getAdlib(int i)
+	{
+		if(i == 0)
+		{
+			String word = converter.getWord("adlib");
+
+			return word;
+		}
+		
+		if(lyrics.get(i - 1).equalsIgnoreCase("adlib"))
+		{
+			String word = converter.getWord("adlib");
+
+			return word;
+		}
+		
+		else if(lyrics.get(i - 1).equalsIgnoreCase("endverse"))
+		{
+			String word = converter.getWord("adlib");
+
+			return word;
+		}
+		
+		else if(lyrics.get(i - 1).equalsIgnoreCase("adlib"))
+		{
+			String word = converter.getWord("adlib");
+
+			return word;
+		}
+		
+		else if(Math.random() > 0.45){
+			return "(" + toReturn.get(i - 1) + ")";
+		}
+		
+		else{
+			String word = converter.getWord("adlib");
+
+			return word;
+		}
+	}
+	
 }
